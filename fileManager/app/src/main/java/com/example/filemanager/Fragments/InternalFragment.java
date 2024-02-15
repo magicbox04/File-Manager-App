186ab2cc0c1c2f30b2d1e0ad25d7950cc1a2d38b
magicbox04 <robin040416@gmail.com>
2024-02-14 19:57:06 -0500

    file Adapter

--- fileManager/app/src/main/java/com/example/filemanager/Fragments/InternalFragment.java
+++ fileManager/app/src/main/java/com/example/filemanager/Fragments/InternalFragment.java
@@ -1,19 +1,47 @@
 package com.example.filemanager.Fragments;
 
+import android.Manifest;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+//import com.example.filemanager.Manifest;
+import com.example.filemanager.FileAdapter;
 import com.example.filemanager.R;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.DexterBuilder;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InternalFragment extends Fragment
 {
+    private RecyclerView recyclerView;
+    private FileAdapter fileAdapter;
+    private List<File> filelist;
+    private ImageView img_back;
+    private TextView tv_pathHolder;
+    File storage;
+
      View view;
+
 
     @Nullable
     @Override
@@ -21,6 +49,82 @@ public class InternalFragment extends Fragment
     {
         view = inflater.inflate(R.layout.fragment_internal,container,false);
 
+        tv_pathHolder = view.findViewById(R.id.tv_pathHolder);
+        img_back = view.findViewById(R.id.img_back);
+
+
+
+        String internalStorage = System.getenv("EXTERNAL_STORAGE");
+        storage = new File(internalStorage);
+
+        tv_pathHolder.setText(storage.getAbsolutePath());
+
+        runtimePermission();
+
         return view;
+    }
+
+    private void runtimePermission()
+    {
+        Dexter.withContext(getContext()).withPermissions(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        ).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport)
+            {
+                displayFiles();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken)
+            {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+    public ArrayList<File> findFiles(File file)
+    {
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] files = file.listFiles();
+
+        for (File singleFile : files)
+        {
+            if (singleFile.isDirectory() && !singleFile.isHidden())
+            {
+                arrayList.add(singleFile);
+            }
+        }
+        for (File singleFile : files)
+        {
+            if (singleFile.getName().toLowerCase().endsWith(".jpeg") ||
+                    singleFile.getName().toLowerCase().endsWith(".png") ||
+                    singleFile.getName().toLowerCase().endsWith(".jpg") ||
+                    singleFile.getName().toLowerCase().endsWith(".mp3") ||
+                    singleFile.getName().toLowerCase().endsWith(".wav") ||
+                    singleFile.getName().toLowerCase().endsWith(".mp4") ||
+                    singleFile.getName().toLowerCase().endsWith(".txt") ||
+                    singleFile.getName().toLowerCase().endsWith(".pdf") ||
+                    singleFile.getName().toLowerCase().endsWith(".doc") ||
+                    singleFile.getName().toLowerCase().endsWith(".apk") ||
+                    singleFile.getName().toLowerCase().endsWith(".docx") ||
+                    singleFile.getName().toLowerCase().endsWith(".webp"))
+            {
+                arrayList.add(singleFile);
+            }
+        }
+        return arrayList;
+    }
+
+    private void displayFiles()
+    {
+        recyclerView = view.findViewById(R.id.recycler_internal);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        filelist = new ArrayList<>();
+        filelist.addAll(findFiles(storage));
+        fileAdapter = new FileAdapter(getContext(), filelist);
+        recyclerView.setAdapter(fileAdapter);
     }
 }
